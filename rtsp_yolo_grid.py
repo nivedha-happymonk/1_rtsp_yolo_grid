2bdad8dde2aeef88a6102225126322c426ff1210
nivedha-hm <132347347+nivedha-happymonk@users.noreply.github.com>
2023-07-18 17:58:31 +0530

    path

--- rtsp_yolo_grid.py
+++ rtsp_yolo_grid.py
@@ -3,7 +3,7 @@ from yolov8 import YOLOv8
 import numpy as np
 import math
 
-model_path = "C:/Users/vijus/Documents/nivedha/Happymonk/yolov8n.onnx"
+model_path = "./yolov8n.onnx"
 yolov8_detector = YOLOv8(model_path, conf_thres=0.2, iou_thres=0.3)
 
 rtsp_links = ['rtsp://happymonk:admin123@streams.ckdr.co.in:1554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif',
